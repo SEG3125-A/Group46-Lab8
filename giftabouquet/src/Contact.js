4438d37c006d7c2def1d61bdc8c4f575c64414eb
mfawaz092 <mfawazph@gmail.com>
2024-03-17 19:54:59 -0400

    contact page

--- giftabouquet/src/Contact.js
+++ giftabouquet/src/Contact.js
@@ -1,15 +1,80 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './styles/ContactCSS.css';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [message, setMessage] = useState('');
+  const [isSubmitEnabled, setIsSubmitEnabled] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+    checkFormValidity();
+  };
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+    checkFormValidity();
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+    checkFormValidity();
+  };
+
+  const checkFormValidity = () => {
+    setIsSubmitEnabled(name !== '' && address !== '' && message.split(/\s+/).length <= 100);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    // Handle form submission
+    console.log("Name:", name);
+    console.log("Address:", address);
+    console.log("Message:", message);
+    // Reset form fields after submission
+    setName('');
+    setAddress('');
+    setMessage('');
+    setIsSubmitEnabled(false);
+    setIsSubmitted(true); // Set submitted state to true
+  };
+
   return (
     <div>
-        Contact 
-        <button>
-            Submit Order
-        </button>
-    </div> 
+      <div className="progress-bar"></div> {/* Progress bar */}
+      <h2> Your Contact Information</h2>
+      <h3> *Text fields are required.</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name: * 
+          <input type="text" value={name} onChange={handleNameChange} placeholder='Enter Full Name' />
+        </label>
+        <br />
+        <br />
+        <label>
+          Address: * 
+          <input type="text" value={address} onChange={handleAddressChange} placeholder='Town/City & Postal Code'/>
+        </label>
+        <br />
+        <br />
+        <label>
+          Message (limit 100 words):
+          <textarea value={message} onChange={handleMessageChange} placeholder='Do you have any questions/feedback for us?' />
+        </label>
+        <br />
+        <button type="submit" disabled={!isSubmitEnabled}>Submit</button>
+        {isSubmitted && (
+          <div style={{ display: 'inline-block', marginLeft: '10px', color: '#d63c96', textAlign: 'center' }}>
+            <h2>Thank You for your interest!</h2>
+            <p>We will be in-contact with you very soon.</p>
+          </div>
+        )}
+      </form>
+    </div>
   );
 }
 
 export default Contact;
+
